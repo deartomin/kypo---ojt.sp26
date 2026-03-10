5eff6faca3ef8384220224488abc689749741ef7
Min <lixhayashi2005@gmail.com>
2026-03-10 12:14:23 +0700

    remove test index

---
+++ .docusaurus/routes.js
@@ -0,0 +1,208 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/kypo---ojt.sp26/__docusaurus/debug',
+    component: ComponentCreator('/kypo---ojt.sp26/__docusaurus/debug', 'f44'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/__docusaurus/debug/config',
+    component: ComponentCreator('/kypo---ojt.sp26/__docusaurus/debug/config', 'c3a'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/__docusaurus/debug/content',
+    component: ComponentCreator('/kypo---ojt.sp26/__docusaurus/debug/content', '9b8'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/__docusaurus/debug/globalData',
+    component: ComponentCreator('/kypo---ojt.sp26/__docusaurus/debug/globalData', 'dcc'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/__docusaurus/debug/metadata',
+    component: ComponentCreator('/kypo---ojt.sp26/__docusaurus/debug/metadata', '533'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/__docusaurus/debug/registry',
+    component: ComponentCreator('/kypo---ojt.sp26/__docusaurus/debug/registry', '8a2'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/__docusaurus/debug/routes',
+    component: ComponentCreator('/kypo---ojt.sp26/__docusaurus/debug/routes', '444'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/blog',
+    component: ComponentCreator('/kypo---ojt.sp26/blog', '1ce'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/blog/archive',
+    component: ComponentCreator('/kypo---ojt.sp26/blog/archive', '126'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/blog/authors',
+    component: ComponentCreator('/kypo---ojt.sp26/blog/authors', '8e0'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/blog/authors/all-sebastien-lorber-articles',
+    component: ComponentCreator('/kypo---ojt.sp26/blog/authors/all-sebastien-lorber-articles', 'e43'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/blog/authors/yangshun',
+    component: ComponentCreator('/kypo---ojt.sp26/blog/authors/yangshun', '253'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/blog/first-blog-post',
+    component: ComponentCreator('/kypo---ojt.sp26/blog/first-blog-post', '1e3'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/blog/long-blog-post',
+    component: ComponentCreator('/kypo---ojt.sp26/blog/long-blog-post', 'c77'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/blog/mdx-blog-post',
+    component: ComponentCreator('/kypo---ojt.sp26/blog/mdx-blog-post', 'a4a'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/blog/tags',
+    component: ComponentCreator('/kypo---ojt.sp26/blog/tags', '4b3'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/blog/tags/docusaurus',
+    component: ComponentCreator('/kypo---ojt.sp26/blog/tags/docusaurus', 'e8a'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/blog/tags/facebook',
+    component: ComponentCreator('/kypo---ojt.sp26/blog/tags/facebook', 'b40'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/blog/tags/hello',
+    component: ComponentCreator('/kypo---ojt.sp26/blog/tags/hello', '8d9'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/blog/tags/hola',
+    component: ComponentCreator('/kypo---ojt.sp26/blog/tags/hola', '4b6'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/blog/welcome',
+    component: ComponentCreator('/kypo---ojt.sp26/blog/welcome', '183'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/markdown-page',
+    component: ComponentCreator('/kypo---ojt.sp26/markdown-page', 'd22'),
+    exact: true
+  },
+  {
+    path: '/kypo---ojt.sp26/docs',
+    component: ComponentCreator('/kypo---ojt.sp26/docs', '553'),
+    routes: [
+      {
+        path: '/kypo---ojt.sp26/docs',
+        component: ComponentCreator('/kypo---ojt.sp26/docs', '7c5'),
+        routes: [
+          {
+            path: '/kypo---ojt.sp26/docs',
+            component: ComponentCreator('/kypo---ojt.sp26/docs', '6c0'),
+            routes: [
+              {
+                path: '/kypo---ojt.sp26/docs/category/tutorial---basics',
+                component: ComponentCreator('/kypo---ojt.sp26/docs/category/tutorial---basics', '83d'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/kypo---ojt.sp26/docs/category/tutorial---extras',
+                component: ComponentCreator('/kypo---ojt.sp26/docs/category/tutorial---extras', '37f'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/kypo---ojt.sp26/docs/intro',
+                component: ComponentCreator('/kypo---ojt.sp26/docs/intro', '8c6'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/kypo---ojt.sp26/docs/tutorial-basics/congratulations',
+                component: ComponentCreator('/kypo---ojt.sp26/docs/tutorial-basics/congratulations', 'cac'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/kypo---ojt.sp26/docs/tutorial-basics/create-a-blog-post',
+                component: ComponentCreator('/kypo---ojt.sp26/docs/tutorial-basics/create-a-blog-post', '798'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/kypo---ojt.sp26/docs/tutorial-basics/create-a-document',
+                component: ComponentCreator('/kypo---ojt.sp26/docs/tutorial-basics/create-a-document', '7c2'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/kypo---ojt.sp26/docs/tutorial-basics/create-a-page',
+                component: ComponentCreator('/kypo---ojt.sp26/docs/tutorial-basics/create-a-page', '713'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/kypo---ojt.sp26/docs/tutorial-basics/deploy-your-site',
+                component: ComponentCreator('/kypo---ojt.sp26/docs/tutorial-basics/deploy-your-site', 'b50'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/kypo---ojt.sp26/docs/tutorial-basics/markdown-features',
+                component: ComponentCreator('/kypo---ojt.sp26/docs/tutorial-basics/markdown-features', 'f3d'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/kypo---ojt.sp26/docs/tutorial-extras/manage-docs-versions',
+                component: ComponentCreator('/kypo---ojt.sp26/docs/tutorial-extras/manage-docs-versions', '092'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/kypo---ojt.sp26/docs/tutorial-extras/translate-your-site',
+                component: ComponentCreator('/kypo---ojt.sp26/docs/tutorial-extras/translate-your-site', 'eb0'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '/kypo---ojt.sp26/',
+    component: ComponentCreator('/kypo---ojt.sp26/', 'da3'),
+    exact: true
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
