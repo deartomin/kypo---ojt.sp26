c0a8502c312712115bd138975924422e2ba2ae43
deartomin <deartomin@users.noreply.github.com>
2026-03-24 13:15:56 +0700

    Fix Swagger spec path for GitHub Pages

--- src/components/SwaggerUIComponent.js
+++ src/components/SwaggerUIComponent.js
@@ -1,9 +1,10 @@
 import React from "react";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 export default function SwaggerUIComponent() {
-    return (
-        <SwaggerUI url="/training-api-docusaurus.yaml" />
-    );
+  const specUrl = useBaseUrl("/training-api-docusaurus.yaml");
+
+  return <SwaggerUI url={specUrl} />;
 }
